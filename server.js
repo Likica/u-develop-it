1526ede448964f611814d746820b42dab66512eb
Coco Puff <fahrija_wyroski@live.com>
2021-05-15 17:23:48 -0500

    connected db

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // add Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+//Connect to dabatabase
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL passwrod
+        password: 'BootSQL#2021',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+// return all the data in the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // // get test route 
 // app.get('/', (req, res) => {
