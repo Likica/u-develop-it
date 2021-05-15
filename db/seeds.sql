e1bbaca1e0d61343476e58644d039d11e2b7a44c
Coco Puff <fahrija_wyroski@live.com>
2021-05-15 15:35:57 -0500

    added tables

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+('Virginia', 'Woolf', 1),
+('Piers', 'GAVESTON', 0),
+('Charles', 'LeRoi', 1),('Katherine', 'Mansfield', 1),
+('Dora', 'Carrington', 0),
+('Edward', 'Bellamy', 0),
+('Montague', 'Summers', 1),
+('Octavia', 'Butler', 1),
+('Unica', 'Zurn', 1);
